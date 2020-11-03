6510b49a653255bcc78ed732787ba92f58429865
Jbeltrez <beltrezjohn@gmail.com>
2020-11-03 05:59:44 -0500

    just threw in some error messages

--- app/controllers/prescription_controller.rb
+++ app/controllers/prescription_controller.rb
@@ -18,6 +18,7 @@ class PrescriptionsController < ApplicationController
     end
 
     get '/prescriptions/new' do
+        if logged_in?
         
          
         # checking if they are logged in 
@@ -29,16 +30,31 @@ class PrescriptionsController < ApplicationController
         #     redirect to :"/login" #redirecting if they arent
         # end 
         erb :'/prescriptions/new'
+        else 
+            flash[:error] = "Please sign up or log in to continue"
+            redirect '/'
+        end 
     end 
     get "/prescriptions/custom" do 
+        if logged_in? 
         erb :'/prescriptions/custom'
+        else 
+            flash[:error] = "Please sign up or log in to continue"
+            redirect '/'
+        end 
     end 
     post "/custom" do 
-        @prescription = Prescription.create(name: params[:name], description: params[:description])
+        if logged_in?
+        @prescription = current_patient.prescriptions.create(name: params[:name], description: params[:description])
         redirect "/prescriptions/#{@prescription.id}"
+        else 
+            flash[:error] = "Please sign up or log in to continue"
+            redirect '/'
+        end 
     end 
 
     post "/prescriptions" do
+        if logged_in?
         params[:effect].downcase
         input = params[:effect].downcase
         effects = ["relaxed", "euphoric", "creative", "happy", "uplifted"]
@@ -51,6 +67,9 @@ class PrescriptionsController < ApplicationController
             flash[:error] = "Please enter one of the following to recieve a prescription: Relaxed, Happy, Euphoric, Uplifted, Creative"
             redirect to '/prescriptions/new', danger: "INvalid"
         end 
+    else 
+        flash[:error] = "Please sign up or log in to continue"
+        redirect '/'
         # binding.pry 
         # if params[:effect] = "relaxed"
         #     flash[:error] = "we cant make you feel that"
@@ -66,29 +85,48 @@ class PrescriptionsController < ApplicationController
     end 
 
     get '/prescriptions/:id' do 
+        if logged_in?
         @prescription = Prescription.find(params[:id])
         erb :'prescriptions/show'
+        else 
+            flash[:error] = "Please log in or sign up to continue"
+            redirect '/'
+        end 
     end 
 
     get '/prescriptions/:id/edit' do 
+        if logged_in?
         @prescription = Prescription.find(params[:id])
         erb :'prescriptions/edit'
+        else 
+            flash[:error] = "Please log in or sign up to continue"
+            redirect '/'
+        end 
 
     end 
 
     patch '/prescriptions/:id' do 
+        if logged_in?
         @prescription = Prescription.find(params[:id])
         @prescription.update(notes: params[:notes], rating: params[:rating])
 
         redirect "/prescriptions"
+        else 
+            flash[:error] = "Please log in or sign up to continue"
+            redirect '/'
         # @prescription = Prescription.find(params[:id])
         
     end 
 
     delete '/prescriptions/:id/delete' do 
+        if logged_in?
         @prescription = Prescription.find(params[:id])
         @prescription.delete 
         redirect '/prescriptions'
+        else 
+            flash[:error] = "Please log in or sign up to continue"
+            redirect '/'
+        end 
     end 
  
 end 
